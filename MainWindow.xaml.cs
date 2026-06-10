4fbf067948a4d258a557502e1a3babb0347459f4
szczman <szymongorczak9@gmail.com>
2026-06-11 00:48:25 +0200

    add

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DagDrop_POPRAWNY
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RedRectangle_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragDrop.DoDragDrop(RedRectangle, RedRectangle, DragDropEffects.Move);
+            }
+        }
+
+        private void Canvas_Drop(object sender, DragEventArgs e)
+        {
+            //Point dropPosition =  e.GetPosition(myCanvas);
+            //Canvas.SetLeft(RedRectangle, dropPosition.X);
+            //Canvas.SetTop(RedRectangle, dropPosition.Y);
+        }
+
+        private void myCanvas_DragOver(object sender, DragEventArgs e)
+        {
+            Point dropPosition = e.GetPosition(myCanvas);
+            Canvas.SetLeft(RedRectangle, dropPosition.X);
+            Canvas.SetTop(RedRectangle, dropPosition.Y);
+        }
+    }
+}
